c22aa2618765745b8cc42b568b5cefcb2cff198a
Vinicius Guarese Caldato <viniciuscaldatoo@gmail.com>
2025-08-25 22:02:23 -0300

    Ajustes

--- src/app.module.ts
+++ src/app.module.ts
@@ -12,7 +12,7 @@ import { ConfigModule } from '@nestjs/config';
       type: 'postgres',
       host: process.env.DB_HOST,
       port: Number(process.env.DB_PORT),
-      username: process.env.DB_USER,
+      username: process.env.DB_USERNAME,
       password: process.env.DB_PASSWORD,
       database: process.env.DB_DATABASE,
       autoLoadEntities: true,
